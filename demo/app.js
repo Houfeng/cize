f92201ff0f196b7f5affb079547d10abe97a6ab9
Houfeng <admin@xhou.net>
2016-06-06 16:26:22 +0800

    dev

--- demo/app.js
+++ demo/app.js
@@ -1,16 +1,22 @@
 var ci = require('../');
-var server = new ci.Server();
+var server = new ci.Server({
+  port: 8090
+});
 
 var demo1 = server.project('demo1', {
   repertory: 'https://github.com/houfeng/cix.git'
 });
 
-demo1.job('build', ci.shell(function () {
-  /*
-  ls
-  */
+demo1.job('pull', function (context, done) {
+  console.log('pull');
+  done();
+});
+
+demo1.job('build', ci.on('pull', function (context, done) {
+  console.log('build');
+  done();
 }));
 
-demo1.invoke('build');
+//demo1.invoke('pull');
 
 server.start();
