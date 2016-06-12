92c05983c79981dd96a3b6f9530d77e7adf3df4f
Houfeng <admin@xhou.net>
2016-06-13 02:21:18 +0800

    dev

--- demo/app.js
+++ demo/app.js
@@ -1,5 +1,6 @@
 var ci = require('../');
 var server = new ci.Server({
+  workspace: '/Users/Houfeng/Desktop/cix-root',
   port: 8090
 });
 
@@ -7,39 +8,19 @@ var demo1 = server.project('demo1', {
   repertory: 'https://github.com/houfeng/cix.git'
 });
 
-demo1.job('pull', ci.series([
-  function (context, done) {
-    context.console.log('pull1');
-    done();
-  },
-  function (context, done) {
-    context.console.log('pull2');
-    done();
-  },
-  ci.shell(function () {
-    /*
-    echo pull3
-    ls
-    */
-  }),
-  'build'
-]));
+demo1.job('pull', ci.series([function (done) {
+  this.console.log('pull1');
+  done();
+}]));
 
-demo1.job('build', ci.on(['pull'], function (context, done) {
-  context.console.log('build');
+demo1.job('build', ci.on(['pull'], function (done) {
+  this.console.log('build');
   done();
 }));
 
-demo1.job('build-prod', ci.shell(() => {
-  /*
-
-  */
-}));
-
-
 server.start(function () {
-  // demo1.invoke('pull', function (err, context) {
-  //   if (err) throw (err);
-  //   console.log(context.path);
-  // });
+  demo1.invoke('pull', function (err, job) {
+    if (err) throw (err);
+    console.log(job.name);
+  });
 });
