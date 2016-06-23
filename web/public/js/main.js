ed03ba6d55a4a36992cd9db45cb2de6fc9e3e82f
Houfeng <admin@xhou.net>
2016-06-23 10:29:41 +0800

    更新 UI

--- web/public/js/main.js
+++ web/public/js/main.js
@@ -1,5 +1,14 @@
 (function ($) {
-  $(document).on('click', '#btn-back', function (event) {
-    history.back();
+
+  /**
+   * 可点击的元素(模拟 a)
+   **/
+  $(document).on('click', '.clickable', function (event) {
+    var link = $(this);
+    var href = link.attr('href');
+    var target = link.attr('target');
+    if (!href) return;
+    window.open(href, target);
   });
+
 })(jQuery);
