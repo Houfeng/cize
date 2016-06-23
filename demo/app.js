e333921742aa9224a11b0c5020237fab664e7241
Houfeng <admin@xhou.net>
2016-06-23 22:09:39 +0800

    生构 cron

--- demo/app.js
+++ demo/app.js
@@ -10,17 +10,22 @@ var demo1 = ci.project('demo1', {
   repertory: 'https://github.com/nokitjs/nokit.git'
 });
 
-demo1.job('pull', ci.shell(function () {
-  /*
-  git clone ${project.options.repertory} ./
-  */
-}));
+// demo1.job('pull', ci.shell(function () {
+//   /*
+//   git clone ${project.options.repertory} ./
+//   */
+// }));
 
-demo1.job('build', ci.on(['pull'], ci.shell(function () {
-  /*
-  npm install
-  npm test
-  */
-})));
+// demo1.job('build', ci.on(['pull'], ci.shell(function () {
+//   /*
+//   npm install
+//   npm test
+//   */
+// })));
+
+demo1.job('cron', ci.cron('*/1 * * * * *', function (done) {
+  console.log('test');
+  done(0);
+}));
 
 ci.start();
