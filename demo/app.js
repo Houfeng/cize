cde44e52a42899590b6f69b6c6b8449b2ebcb274
Houfeng <admin@xhou.net>
2016-06-25 14:46:23 +0800

    UI

--- demo/app.js
+++ demo/app.js
@@ -6,22 +6,23 @@ ci.init({
   secret: '12345',
 });
 
-// var demo1 = ci.project('demo1', {
-//   repertory: 'https://github.com/nokitjs/nokit.git'
-// });
+var demo1 = ci.project('demo1', {
+  repertory: 'https://github.com/nokitjs/nokit.git'
+});
 
-// demo1.job('pull', ci.shell(function () {
-//   /*
-//   echo pull
-//   */
-// }));
+demo1.job('pull', ci.shell(function () {
+  /*
+  git clone https://github.com/nokitjs/nokit.git ./
+  npm i
+  */
+}));
 
-// demo1.job('build', ci.by(['pull'], ci.shell(function () {
-//   /*
-//   echo build
-//   */
-// })));
+demo1.job('build', ci.by(['pull'], ci.shell(function () {
+  /*
+  npm test
+  */
+})));
 
-// demo1.job('cron', ci.cron('*/10 * * * * *', 'pull'));
+//demo1.job('cron', ci.cron('*/10 * * * * *', 'pull'));
 
 ci.start();
--- web/controllers/main.js
+++ web/controllers/main.js
@@ -1,3 +1,7 @@
+const async = require('async');
+const fs = require('fs');
+const path = require('path');
+
 /**
  * 定义 MainController
  **/
@@ -6,10 +10,29 @@ const MainController = nokit.define({
   /**
    * 初始化方法，每次请求都会先执行 init 方法
    **/
-  init: function () {
+  init: function* () {
     var self = this;
-    //CI
     self.ci = self.server.ci;
+    async.series([
+      self._loadProjects.bind(self),
+      self._loadJobs.bind(self),
+      self._loadRecords.bind(self)
+    ], function (err) {
+      if (err) return self.context.error(err);
+      self.sn = self.context.params.sn;
+      if (self.sn) {
+        self._loadOneRecord(self.sn, self.ready.bind(self));
+      } else {
+        self.ready();
+      }
+    });
+  },
+
+  /**
+   * 加载项目
+   **/
+  _loadProjects: function (callback) {
+    var self = this;
     //项目
     self.projects = self.ci.projects;
     self.projectName = self.context.params.project || Object.getOwnPropertyNames(self.projects)[0];
@@ -20,6 +43,14 @@ const MainController = nokit.define({
     if (!self.project) {
       return self.context.notFound();
     }
+    if (callback) callback();
+  },
+
+  /**
+   * 加载 Job
+   **/
+  _loadJobs: function (callback) {
+    var self = this;
     //job
     self.jobs = self.project.jobs;
     self.jobName = self.context.params.job || Object.getOwnPropertyNames(self.jobs)[0];
@@ -30,12 +61,37 @@ const MainController = nokit.define({
     if (!self.job) {
       return self.context.notFound();
     }
-    //记录
-    self.sn = self.context.params.sn;
+    if (callback) callback();
+  },
+
+  /**
+   * 加载记录
+   **/
+  _loadRecords: function (callback) {
+    var self = this;
     self.job.getRecords(100, 0, function (err, records) {
       if (err) return self.context.error(err);
       self.records = records;
-      self.ready();
+      if (callback) callback();
+    });
+  },
+
+  /**
+   * 加载一条记录
+   **/
+  _loadOneRecord: function (sn, callback) {
+    var self = this;
+    self.job.getRecord(sn, function (err, record) {
+      if (err) return callback(err);
+      if (!record) return self.context.notFound();
+      var contextPaths = self.ci.getContextPaths(record.contextId);
+      var outFile = path.normalize(`${contextPaths.out}/${record._id}.txt`);
+      fs.readFile(outFile, function (err, data) {
+        if (err) return callback(err);
+        record.out = data.toString();
+        self.record = record;
+        callback();
+      });
     });
   },
 
