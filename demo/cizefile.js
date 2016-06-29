8332c71b73d2708d47014b86fd0551ded539fbad
Houfeng <admin@xhou.net>
2016-06-30 00:07:41 +0800

    0.0.13 更新名称验证

--- demo/cizefile.js
+++ demo/cizefile.js
@@ -9,7 +9,7 @@ module.exports = function (ci) {
     repertory: 'git@10.211.55.8:houfeng/test.git'
   });
   
-  test.job('PULL-PR', function(done){
+  test.job('PULLPR', function(done){
     this.console.log(this.params);
     this.context.summary = `MR #${this.params.object_attributes.id},${this.params.object_attributes.source_branch}->${this.params.object_attributes.target_branch}`;
     done();
