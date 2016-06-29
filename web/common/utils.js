9d62ea24b176ec45fe822ce80f8759ea8723110f
Houfeng <admin@xhou.net>
2016-06-29 13:16:48 +0800

    重构状态图标显示

--- web/common/utils.js
+++ web/common/utils.js
@@ -1,4 +1,14 @@
 const utils = nokit.utils;
 utils.pkg = require('../../package.json');
 
+utils.init = function (server) {
+  //noop
+};
+
+utils.statusIcons = {
+  "300": "fa-exclamation-circle",
+  "200": "fa-check-circle",
+  "100": "fa-spinner fa-spin fa-fw"
+};
+
 module.exports = utils; 
--- web/global.js
+++ web/global.js
@@ -1,3 +1,5 @@
+var utils = require('./common/utils');
+
 /**
  * 全局应用程序类
  **/
@@ -7,6 +9,7 @@ const Global = nokit.define({
    * 在 server 启动时
    **/
   onStart: function (server, done) {
+    utils.init(server);
     done();
   },
 
