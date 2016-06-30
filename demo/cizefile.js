ec5a1779fc2ff56ef0ca2f3ae9f5c6f7a8686a93
Houfeng <admin@xhou.net>
2016-06-30 12:22:34 +0800

    重构 runable ，改变第一个参数为 self

--- demo/cizefile.js
+++ demo/cizefile.js
@@ -8,10 +8,10 @@ module.exports = function (ci) {
   const test = ci.project('test', {
     repertory: 'git@10.211.55.8:houfeng/test.git'
   });
-  
-  test.job('PULLPR', function(done){
-    this.console.log(this.params);
-    this.context.summary = `MR #${this.params.object_attributes.id},${this.params.object_attributes.source_branch}->${this.params.object_attributes.target_branch}`;
+
+  test.job('PULLPR', function (self) {
+    self.console.log(self.params);
+    self.context.summary = `MR #${self.params.object_attributes.id},${self.params.object_attributes.source_branch}->${self.params.object_attributes.target_branch}`;
     done();
   });
 
