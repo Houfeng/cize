23144323a3b54bea7ce58b3f779c649020b65e2c
Houfeng <admin@xhou.net>
2016-07-04 00:34:04 +0800

    添加部分单元测试

--- test/server.test.js
+++ test/server.test.js
@@ -1,14 +1,55 @@
 var assert = require('assert');
+var rmdir = require('rmdir');
 var ci = require('../');
 
 describe('server', function () {
+
+  var workspace = `${__dirname}/.workspace`;
+
+  var testProject, testJob, testInstance, testParams;
+  beforeEach(function () {
+    ci.config({
+      port: 8008,
+      workspace: workspace
+    });
+    testProject = ci.project('test', {
+      git: '/test.git'
+    });
+    testProject.job('test', function (self) {
+      testInstance = self;
+      testParams = self.params;
+      self.done();
+    });
+    ci.start();
+  });
+
   describe('#config()', function () {
     it('init by options', function () {
-      ci.config({
-        workspace: '/test'
-      });
-      assert.equal(ci.paths.data, '/test/data/');
-      assert.equal(ci.paths.works, '/test/works/');
+      assert.equal(ci.paths.data, `${workspace}/data/`);
+      assert.equal(ci.paths.works, `${workspace}/works/`);
+      assert.equal(ci.options.port, 8008);
     });
   });
+
+  describe('#project()', function () {
+    it('define project', function () {
+      assert.equal(testProject.name, 'test');
+      assert.equal(testProject.options.git, '/test.git');
+    });
+  });
+
+  describe('#invoke()', function () {
+    it('define project', function (done) {
+      ci.invoke('test', 'test', { test: true }, function () {
+        assert.equal(testInstance.name, 'test');
+        assert.equal(testParams.test, true);
+        done();
+      });
+    });
+  });
+
+  afterEach(function () {
+    ci.stop();
+  });
+
 });
