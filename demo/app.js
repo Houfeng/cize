3defaff699ff9b4cc16573fe8e699c26cb512bac
Houfeng <admin@xhou.net>
2016-07-13 20:32:56 +0800

    改进队列 Job

--- demo/app.js
+++ demo/app.js
@@ -10,11 +10,13 @@ var demo1 = ci.project('demo1', {
   repertory: 'https://github.com/nokitjs/nokit.git'
 });
 
-demo1.job('test', ci.series([ci.shell(function () {
-  /*
-  echo test
-  */
-})]));
+demo1.job('test', ci.parallel(function () {
+  this.console.error('1');
+  this.done(new Error(0));
+}, function () {
+  this.console.error('2');
+  this.done();
+}));
 
 
 ci.start();
