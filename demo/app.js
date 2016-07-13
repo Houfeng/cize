32776c9df4868b6fbcf79dd0d639a5c8836d18e4
Houfeng <admin@xhou.net>
2016-07-13 19:36:37 +0800

    改进标准输出

--- demo/app.js
+++ demo/app.js
@@ -10,39 +10,11 @@ var demo1 = ci.project('demo1', {
   repertory: 'https://github.com/nokitjs/nokit.git'
 });
 
-demo1.job('pull', ci.shell(function () {
+demo1.job('test', ci.series([ci.shell(function () {
   /*
-  #git clone https://github.com/nokitjs/nokit.git ./
-  #npm i
-  #echo pull
+  echo test
   */
-}));
+})]));
 
-demo1.job('build', ci.by(['pull'], ci.shell(function () {
-  /*
-  nokit xxx
-  npm test
-  */
-})));
-
-demo1.job('test', ci.shell(function () {
-  /*
-  nokit test
-  ping baidu.com
-  */
-})).beforeRun(function (job) {
-  return false;
-});
-
-demo1.job('s', ci.parallel([
-  function (self) {
-    self.done(new Error('1'));
-  },
-  function (self) {
-    self.done(new Error('2'));
-  }
-]))
-
-//demo1.job('cron', ci.cron('*/2 * * * * *', 'build'));
 
 ci.start();
